8d6b9d200fb5c4d651569c31ef0ce3495fba53c4
chrisrabe <chris.rabe14@gmail.com>
2023-12-31 00:27:27 +1000

    feat: removed a bunch of code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react"
 import reactLogo from "./assets/rune.svg"
 import viteLogo from "/vite.svg"
-import "./App.css"
 import { GameState } from "./logic.ts"
 
 function App() {
   const [game, setGame] = useState<GameState>()
+
   useEffect(() => {
     Rune.initClient({
       onChange: ({ game }) => {
@@ -19,29 +19,7 @@ function App() {
   }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://developers.rune.ai" target="_blank">
-          <img src={reactLogo} className="logo rune" alt="Rune logo" />
-        </a>
-      </div>
-      <h1>Vite + Rune</h1>
-      <div className="card">
-        <button onClick={() => Rune.actions.increment({ amount: 1 })}>
-          count is {game.count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> or <code>src/logic.ts</code> and save to
-          test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and Rune logos to learn more
-      </p>
-    </>
+    <div>hello world</div>
   )
 }
 
